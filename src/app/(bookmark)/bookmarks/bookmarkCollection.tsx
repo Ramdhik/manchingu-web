c7b79429441ea893b7adfda6b90bfc8d625de756
onesusnayir <senoriyan22@gmail.com>
2025-07-31 15:44:38 +0700

    add active class to btn

--- src/app/(bookmark)/bookmarks/bookmarkCollection.tsx
+++ src/app/(bookmark)/bookmarks/bookmarkCollection.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export default function BookmarkCollection({ data }: { data: Data[] }) {
     const [bookmarks, setBookmarks] = useState<Data[]>(data)
+    const [ filter, setFilter ] = useState<string>("COMPLETED")
 
     const BookmarksGrid = bookmarks.map((item) => {
         return (
@@ -17,15 +18,16 @@ export default function BookmarkCollection({ data }: { data: Data[] }) {
     const handleOnClick = (filter: string) => {
         const newBookmarks = data.filter((bookmark) => bookmark.status === filter)
         setBookmarks(newBookmarks)
+        setFilter(filter)
     }
 
     return(
         <section>
-            <div className="p-5">
-                <Button onClick={() => handleOnClick("COMPLETED")}>Complete</Button>
-                <Button onClick={() => handleOnClick("READING")}>Reading</Button>
-                <Button onClick={() => handleOnClick("DROPPED")}>Dropped</Button>
-                <Button onClick={() => handleOnClick("PLAN_TO_READ")}>Plan To Read</Button>
+            <div className="p-5 flex gap-3">
+                <Button onClick={() => handleOnClick("COMPLETED")} className={`${filter === "COMPLETED" && "active"} hover:bg-accent`}>Complete</Button>
+                <Button onClick={() => handleOnClick("READING")} className={`${filter === "READING" && "active"} hover:bg-accent`}>Reading</Button>
+                <Button onClick={() => handleOnClick("DROPPED")} className={`${filter === "DROPPED" && "active"} hover:bg-accent`}>Dropped</Button>
+                <Button onClick={() => handleOnClick("PLAN_TO_READ")} className={`${filter === "PLAN_TO_READ" && "active"} hover:bg-accent`}>Plan To Read</Button>
             </div>
             <div className="p-5 grid grid-cols-5 gap-5">
                 {BookmarksGrid}
