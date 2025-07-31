2443232fbc4e32a534be6e6e512565b57cdefe15
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-07-31 15:10:39 +0700

    Add comic card to dashboard

--- src/app/(dashboard)/data/index.ts
+++ src/app/(dashboard)/data/index.ts
@@ -1,23 +1,24 @@
-import { Comic } from './definition';
+import { Comic } from "./definition";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function fetchAllComics(): Promise<Comic[]> {
   if (!BASE_URL) {
-    throw new Error('BASE_URL tidak ditemukan. Periksa file .env');
+    throw new Error("BASE_URL tidak ditemukan. Periksa file .env");
   }
 
   const response = await fetch(`${BASE_URL}/comic/all`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   if (!response.ok) {
-    throw new Error('Gagal mengambil daftar komik');
+    throw new Error("Gagal mengambil daftar komik");
   }
 
-  const comics: Comic[] = await response.json();
+  const json = await response.json();
+  const comics: Comic[] = json.data.items;
   return comics;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,41 @@
-import Image from 'next/image';
-import { Navbar } from './(dashboard)/navbar';
-import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import Image from "next/image";
+import { Navbar } from "./(dashboard)/navbar";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { fetchAllComics } from "./(dashboard)/data";
+import { Comic } from "./(dashboard)/data/definition";
 
 export default async function Home() {
+  const comics: Comic[] = await fetchAllComics();
+
   return (
     <div className="min-h-screen bg-primary">
       <Navbar />
-      <div className="flex flex-col justify-center py-2 bg-primary">
+      <div className="flex flex-col justify-center py-2 bg-primary max-w-7xl mx-auto">
+        <h1 className="text-2xl font-bold text-primary-foreground px-4 mb-4">
+          Popular Titles
+        </h1>
         <Slider />
+        <h1 className="text-2xl font-bold text-primary-foreground px-4 mt-6">
+          Recomended Comic
+        </h1>
         {/* Tambahkan Card di sini */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mt-8 px-4">
-          <CardComponent title="Card 1" description="Deskripsi card pertama" content="Konten card pertama" />
-          <CardComponent title="Card 2" description="Deskripsi card kedua" content="Konten card kedua" />
-          <CardComponent title="Card 3" description="Deskripsi card ketiga" content="Konten card ketiga" />
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-6 px-4">
+          {comics.slice(0, 3).map((comic) => (
+            <CardComponent
+              key={comic.id_comic}
+              title={comic.name}
+              description={comic.synopsis}
+              content={`Rating: ${comic.rating}`}
+              poster={comic.poster}
+            />
+          ))}
         </div>
       </div>
     </div>
@@ -27,16 +50,42 @@ async function Slider() {
   );
 }
 
-async function CardComponent({ title, description, content }: { title: string; description: string; content: string }) {
+async function CardComponent({
+  title,
+  description,
+  content,
+  poster,
+}: {
+  title: string;
+  description: string;
+  content: string;
+  poster: string;
+}) {
   return (
-    <Card className="w-full h-min bg-background shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>{content}</p>
-      </CardContent>
+    // <Card className="w-full h-min bg-background shadow-lg hover:shadow-xl transition-shadow duration-300">
+    //   <CardHeader>
+    //     <Image
+    //       src={poster}
+    //       alt={title}
+    //       width={300}
+    //       height={400}
+    //       className="rounded-lg w-full h-auto object-cover mb-2"
+    //     />
+    //   </CardHeader>
+    //   <CardContent>
+    //     <CardTitle>{title}</CardTitle>
+    //     {/* <CardDescription>{description.slice(0, 80)}...</CardDescription>
+    //     <p className="text-sm text-muted-foreground">{content}</p> */}
+    //   </CardContent>
+    // </Card>
+
+    <Card className="relative w-full aspect-[2/3] bg-transparent shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden border-none">
+      <Image src={poster} alt={title} fill className="object-cover" />
+
+      {/* Overlay judul */}
+      <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/80 via-black/50 to-transparent p-3">
+        <h3 className="text-white text-lg font-semibold">{title}</h3>
+      </div>
     </Card>
   );
 }
